712e6b035cddd28198c2882d11645da69cb82910
Freedesign93 <93400285+Freedesign93@users.noreply.github.com>
2022-03-29 18:27:04 +0700

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6242ebe666467900207141a0",
+				"6242ebe666467900207141a3"
+			]
+		},
+		"6242ebe666467900207141a0": {
+			"id": "6242ebe666467900207141a0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6242ebe666467900207141a3": {
+			"id": "6242ebe666467900207141a3",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
